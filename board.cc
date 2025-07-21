8e28b56d55ce717c336c2168dbd6e45f62f5a0eb
Owen Zhang <67778083+owenz0523@users.noreply.github.com>
2025-07-21 02:28:00 -0400

    board

---
+++ board.cc
@@ -0,0 +1,7 @@
+#include "board.h"
+
+Board::Board() = default;
+
+Board::~Board() = default;
+
+void Board::trigger(Triggers trigger) {}
---
+++ board.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "card_collection.h"
+#include "enums.h"
+
+class Board : public CardCollection {
+    public:
+        Board() = default;
+        ~Board() = default;
+        
+        void trigger(Triggers trigger);
+};
+
+#endif
