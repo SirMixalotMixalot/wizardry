67967d8d51b229607895aa389af4553c4bf2bb01
Owen Zhang <67778083+owenz0523@users.noreply.github.com>
2025-07-22 18:53:02 -0400

    textdisplay up

---
+++ textualdisplay.h
@@ -0,0 +1,17 @@
+#ifndef TEXTUALDISPLAY_H
+#define TEXTUALDISPLAY_H
+
+#include "view.h"
+
+class TextualDisplay : public View {
+    public:
+        TextualDisplay() = default;
+        ~TextualDisplay() = default;
+        
+        void help() override;
+        void inspect(const Minion& minion) override;
+        void showBoard(const Game& game) override;
+        void showHand(const Hand& hand) override;
+};
+
+#endif
