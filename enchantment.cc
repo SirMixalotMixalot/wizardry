27fdfad74f5ec0ea7564872ac946b71e51a52353
umarm105 <paalandtapin@gmail.com>
2025-07-22 20:55:58 -0400

    base enchantment class implemented

---
+++ enchantment.cc
@@ -0,0 +1,47 @@
+#include "enchantment.h"
+
+Enchantment::Enchantment(string name, Player* owner, string description, int cost, string attackModifier, string defenseModifier, Minion* next) : Minion(name, owner, description, cost, 0, 0), attackModifier(attackModifier), defenseModifier(defenseModifier), next(next) {}
+
+string Enchantment::getAttackModifier() const {
+    return attackModifier;
+}
+
+string Enchantment::getDefenseModifier() const {
+    return defenseModifier;
+}
+
+void Enchantment::use() {
+    next->use();
+}
+
+void Enchantment::use(int index, char target) {
+    next->use(index, target);
+}
+
+void Enchantment::restoreActions() {
+    next->restoreActions();
+}
+
+int Enchantment::getAttack() const {
+    return next->getAttack();
+}
+
+int Enchantment::getDefense() const {
+    return next->getDefense();
+}
+
+int Enchantment::getActions() const {
+    return next->getActions();
+}
+
+void Enchantment::setAttack(int attack) {
+    next->setAttack(attack);
+}
+
+void Enchantment::setDefense(int defense) {
+    next->setDefense(defense);
+}
+
+void Enchantment::setActions(int actions) {
+    next->setActions(actions);
+}
