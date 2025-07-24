1022554c61f355d6b3f3087c12c8bc3548ebc81a
umustafa <paalandtapin@gmail.com>
2025-07-24 19:20:11 -0400

    added abstract class to damage minion activated ability to make it more dynamic for future use

---
+++ abilities/activatedabilities/novicepyromancerability.h
@@ -0,0 +1,12 @@
+#ifndef NOVICEPYROMANCERABILITY_H
+#define NOVICEPYROMANCERABILITY_H
+
+#include "damageminion.h"
+
+class NovicePyromancerAbility : public DamageMinion {
+    public:
+        NovicePyromancerAbility();
+        ~NovicePyromancerAbility() override = default;
+};
+
+#endif
