a6a888fd51d80c93e17af812e09321ae22bcc13e
umustafa <paalandtapin@gmail.com>
2025-07-24 17:07:47 -0400

    first concrete activated ability started

---
+++ abilities/activatedabilities/activatedminiondamage.h
@@ -0,0 +1,15 @@
+#ifndef ACTIVATEDMINIONDAMAGE_H
+#define ACTIVATEDMINIONDAMAGE_H
+
+#include "activatedabilities.h"
+
+class ActivatedMinionDamage : public ActivatedAbility {
+    public:
+        ActivatedMinionDamage();
+        ~ActivatedMinionDamage() override = default;
+        
+        unique_ptr<Command> use() override;
+        unique_ptr<Command> use(int player, int targetCard) override;
+};
+
+#endif
