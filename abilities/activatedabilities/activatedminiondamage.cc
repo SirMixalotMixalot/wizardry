ffe1504da72e5f8c68ce4e347447a4c09b95f7c7
umustafa <paalandtapin@gmail.com>
2025-07-24 18:27:03 -0400

    implemented one concrete activated ability and corresponding command

--- abilities/activatedabilities/activatedminiondamage.cc
+++ abilities/activatedabilities/activatedminiondamage.cc
@@ -1,5 +1,5 @@
 #include "activatedminiondamage.h"
-#include "attackcommand.h"
+#include "damageminioncommand.h"
 
 ActivatedMinionDamage::ActivatedMinionDamage() : ActivatedAbility("Deal 1 damage to target minion", 1) {}
 
@@ -8,6 +8,6 @@ unique_ptr<Command> ActivatedMinionDamage::use() {
 }
 
 unique_ptr<Command> ActivatedMinionDamage::use(int player, int targetCard) {
-    return make_unique<AttackCommand>(player - 1, targetCard - 1); // Assumed that you need to substrct 1 because AttackCommand takes indices. Change implementation later if needed.
+    bool isPlayer1 = (player == 1);
+    return make_unique<DamageMinionCommand>(targetCard, isPlayer1);
 }
-// NOTE: just realized AttackCommand starts a clash, but we just need to deal 1 damage to the target. Need to make a new command.
