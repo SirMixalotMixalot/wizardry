6e5c344cc3ba55317d40a1568d3b48b80e50f253
umustafa <paalandtapin@gmail.com>
2025-07-25 01:27:17 -0400

    changed minion.h to include activated abilities, modified subclasses accordingly

--- minions/minion.h
+++ minions/minion.h
@@ -11,8 +11,11 @@ class Minion : public Card {
     int attack;
     int defense;
     int actions;
+<<<<<<< HEAD
     unique_ptr<TriggeredAbility> triggeredAbility;
     unique_ptr<ActivatedAbility> activatedAbility;
+=======
+>>>>>>> a3669e6 (changed minion.h to include activated abilities, modified subclasses accordingly)
 
     public:
         Minion(string name, Player* owner, string description, int cost, int attack, int defense, unique_ptr<ActivatedAbility> activatedAbility);
