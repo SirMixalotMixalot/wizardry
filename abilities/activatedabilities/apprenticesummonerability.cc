0d079dff324db6df81721e46e2ee37c7d0daf4d6
umustafa <paalandtapin@gmail.com>
2025-07-24 21:29:44 -0400

    added concrete abilities for minions that use activated summon abilities

---
+++ abilities/activatedabilities/apprenticesummonerability.cc
@@ -0,0 +1,3 @@
+#include "apprenticesummonerability.h"
+
+ApprenticeSummonerAbility::ApprenticeSummonerAbility() : SummonMinions("Summon a 1/1 air elemental", 1, "Air Elemental", 1) {}
---
+++ abilities/activatedabilities/mastersummonerability.cc
@@ -0,0 +1,3 @@
+#include "mastersummonerability.h"
+
+MasterSummonerAbility::MasterSummonerAbility() : SummonMinions("Summon up to three 1/1 air elementals", 2, "Air Elemental", 3) {}
