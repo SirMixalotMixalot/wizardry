9de204e4675be2cc8bd72998cca8d964802c636b
Owen Zhang <67778083+owenz0523@users.noreply.github.com>
2025-07-28 19:24:04 -0400

    fixed segfault issue

--- commands/damagecommand.cc
+++ commands/damagecommand.cc
@@ -10,10 +10,12 @@ void DamageCommand::execute(Game& game) {
     Player* inactivePlayer = game.getInactivePlayer();
 
     if (owner == inactivePlayer) {
-        int index = activePlayer->getBoard()->getSize() - 1;
-        activePlayer->getMinion(index)->setDefense(activePlayer->getMinion(index)->getDefense() - damage);
-        if (activePlayer->getMinion(index)->getDefense() <= 0) {
-            activePlayer->killMinion(index);
+        if (activePlayer->getBoard()->getSize() > 0) {
+            int index = activePlayer->getBoard()->getSize() - 1;
+            activePlayer->getMinion(index)->setDefense(activePlayer->getMinion(index)->getDefense() - damage);
+            if (activePlayer->getMinion(index)->getDefense() <= 0) {
+                activePlayer->killMinion(index);
+            }
         }
     }  
 }
