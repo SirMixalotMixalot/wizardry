2ef52a8088119353c818302fd051e8787a5874fa
Bokang Keith Mogomotsi <bokangmg24@gmail.com>
2025-07-28 18:57:01 -0400

    recharge description

--- spells/recharge.h
+++ spells/recharge.h
@@ -7,7 +7,7 @@
 class Recharge : public Spell {
 public:
     Recharge(Player* owner)
-        : Spell("Recharge", owner, "Recharge your ritual by 3 charges", 1) {}
+        : Spell("Recharge", owner, "Your ritual gains 3 charges", 1) {}
 
     unique_ptr<Command> use(int player, int targetCard) override {
         throw runtime_error("Recharge spell has no targets.");
