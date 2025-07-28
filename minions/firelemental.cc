3ef330954d44683d3a7980ac13ddd1c29dea30b6
Owen Zhang <67778083+owenz0523@users.noreply.github.com>
2025-07-27 20:32:39 -0400

    fire elemental done

--- minions/firelemental.cc
+++ minions/firelemental.cc
@@ -1,9 +1,11 @@
 #include "firelemental.h"
+#include "damagetrigger.h"
 
 using namespace std;
 
 FireElemental::FireElemental(Player* owner) 
-    : Minion("Fire Elemental", owner, "", 2, 2, 3, nullptr) {}
+    : Minion("Fire Elemental", owner, "", 2, 2, 3, nullptr,
+    make_unique<DamageTrigger>("Whenever an opponent's minion enters play, deal 1 damage to it.", Triggers::MINION_ENTERS_PLAY, owner, 1)) {}
 
 bool FireElemental::canUseAbilities() const {
     return true;
