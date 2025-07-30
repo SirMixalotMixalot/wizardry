e6b0166f95bd1aaeafa79ef7de1eb107d307b82b
Owen Zhang <owen.z.0523@gmail.com>
2025-07-29 20:46:00 -0400

    fix for bone golem

--- abilities/triggeredabilities/miniongainstatstrigger.cc
+++ abilities/triggeredabilities/miniongainstatstrigger.cc
@@ -2,13 +2,26 @@
 #include "gainstatscommand.h"
 #include "minion.h"
 #include "player.h"
+#include "enchantment.h"
+#include <iostream>
+using namespace std;
 
 MinionGainStatsTrigger::MinionGainStatsTrigger(string description, Triggers trigger, Minion* minion, int attack, int defense) : 
     TriggeredAbility(description, trigger), minion(minion), attack(attack), defense(defense) {}
 
 unique_ptr<Command> MinionGainStatsTrigger::use() {
+    cout << "hello" << endl;
     for (int i = 0; i < minion->getOwner()->getBoard()->getSize(); i++) {
-        if (minion->getOwner()->getMinion(i) == minion) {
+        cout << minion->getName() << minion << endl;
+        cout << i << minion->getOwner()->getMinion(i) << minion->getOwner()->getMinion(i)->getName() << endl;
+        const Minion* currentMinion = minion->getOwner()->getMinion(i);
+        // get to base
+        while (auto enchantment = dynamic_cast<const Enchantment*>(currentMinion)) {
+            currentMinion = enchantment->getNext();
+        }
+        if (currentMinion == minion) {
+            cout << minion->getName() << minion << endl;
+            cout << i << minion->getOwner()->getMinion(i) << minion->getOwner()->getMinion(i)->getName() << endl;
             return make_unique<GainStatsCommand>(attack, defense, i, minion->getOwner());
         }
     }
